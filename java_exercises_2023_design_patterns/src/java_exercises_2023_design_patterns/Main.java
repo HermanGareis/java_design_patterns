5aaa37deed4d15e98d17fafbd52f8a6d404e6829
Utente <Utente@192.168.1.3>
2023-04-24 13:55:55 +0200

    Initial Commit

--- java_exercises_2023_design_patterns/src/java_exercises_2023_design_patterns/Main.java
+++ java_exercises_2023_design_patterns/src/java_exercises_2023_design_patterns/Main.java
@@ -1,6 +1,5 @@
 package java_exercises_2023_design_patterns;
 
-
 import decorator.Cheese;
 import decorator.Pepperoni;
 import decorator.Pizza;
@@ -42,31 +41,30 @@ public class Main {
 		System.out.println("Pizza cost: " + pizza.getCost());
 
 		System.out.println();
-		
-		
-		//Factory Client
+
+		// Factory Client
 		System.out.println("Factory: ");
-        ShapeFactory factory = new CircleFactory();
-        Shape shape = factory.createShape();
-        shape.draw();
-        
-        factory = new RectangleFactory();
-        shape = factory.createShape();
-        shape.draw();
-		
-        System.out.println();
-        
-        //Singleton
-        System.out.println("Singleton:");
-        Singleton instance1 = Singleton.getInstance();
-        Singleton instance2 = Singleton.getInstance();
-        if (instance1 == instance2) {
+		ShapeFactory factory = new CircleFactory();
+		Shape shape = factory.createShape();
+		shape.draw();
+
+		factory = new RectangleFactory();
+		shape = factory.createShape();
+		shape.draw();
+
+		System.out.println();
+
+		// Singleton
+		System.out.println("Singleton:");
+		Singleton instance1 = Singleton.getInstance();
+		Singleton instance2 = Singleton.getInstance();
+		if (instance1 == instance2) {
 			System.out.println("They are the same instance");
 		}
-        
-        System.out.println();
 
-		//Dependency Injection
+		System.out.println();
+
+		// Dependency Injection
 		System.out.println("Dependency Injection: ");
 		ServiceDI service = new ServiceImplementation();
 		ClientDI client = new ClientDI(service);
